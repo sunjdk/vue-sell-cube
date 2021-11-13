6512cb8a4eeaf7df3a7441c107ee827236412ef9
桃谷六仙 <jdk_mail@163.com>
2021-11-14 02:11:45 +0800

    修改商品评价的小bug

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -45,7 +45,7 @@
               @toggle="onToggle"
             ></rating-select>
             <div class="rating-wrapper">
-              <ul v-show="ratings && ratings.length">
+              <ul v-show="computedRatings && computedRatings.length">
                 <li v-for="(rating,index) in computedRatings" class="rating-item border-bottom-1px" :key="index">
                   <div class="user">
                     <span class="name">{{rating.username}}</span>
@@ -58,7 +58,7 @@
                   </p>
                 </li>
               </ul>
-              <div class="no-rating" v-show="!ratings || !ratings.length">暂无评价</div>
+              <div class="no-rating" v-show="!computedRatings || !computedRatings.length">暂无评价</div>
             </div>
           </div>
         </div>
