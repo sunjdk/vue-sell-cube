3d699e3c0d47ccdd7eb162f0ad42d6227cd85ce8
桃谷六仙 <jdk_mail@163.com>
2021-11-14 11:32:10 +0800

    开发评论功能

--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,6 @@ import { get } from './helper'
 
 const getSeller = get('/api/seller')
 const getGoods = get('/api/goods')
+const getRatings = get('/api/ratings')
 
-export { getSeller, getGoods }
+export { getSeller, getGoods, getRatings }
